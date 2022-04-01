25f36c909d91a07241b047ad13aa1e6b38204a65
jonahliu0426 <jia7833@hotmail.com>
2022-04-01 19:36:19 -0400

    change style and logo

--- src/components/shared/Seo.js
+++ src/components/shared/Seo.js
@@ -2,7 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 
 function SEO({ title }) {
-  const titleText = title ? `${title} • Instagram` : `Instagram`;
+  const titleText = title ? `${title} • Enter / Space` : `Enter / Space`;
   return (
     <Helmet>
       <title>{titleText}</title>
