bc506d9f6449e7d835390174a25c3b369a8bd16f
jonahliu0426 <jia7833@hotmail.com>
2022-04-01 20:13:32 -0400

    fix color and styles

--- src/styles.js
+++ src/styles.js
@@ -153,12 +153,13 @@ export const useSignUpPageStyles = makeStyles({
     height: "100vh"
   },
   cardHeader: {
-    backgroundImage: `url(${IconSheet})`,
+    backgroundImage: `url(${customizedLogo})`,
+    width: "245px",
+    height: "51px",
+    marginTop: "20px",
     backgroundRepeat: "no-repeat",
-    backgroundPosition: "-98px 0",
-    height: 51,
-    width: 175,
-    margin: "22px auto 12px"
+    backgroundPosition: "23px 3px",
+    backgroundSize: "221px",
   },
   cardHeaderSubHeader: {
     textAlign: "center",
@@ -231,7 +232,7 @@ export const useNavbarStyles = makeStyles(theme => ({
   logo: {
     marginTop: 7,
     maxHeight: "100%",
-    maxWidth: "100%",
+    maxWidth: "68.5%",
     objectFit: "contain"
   },
   input: {
